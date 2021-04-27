c28a8866633f79b867d5ce206c31d7d8281e9344
Schifi <felix-schaefer@hotmail.de>
2021-04-27 12:14:05 +0200

    Added CardFileException for later use

---
+++ src/main/java/de/schifi/cahcardgen/generator/CardFileException.java
@@ -0,0 +1,13 @@
+package de.schifi.cahcardgen.generator;
+
+public class CardFileException extends Exception {
+
+    public CardFileException() {
+
+    }
+
+    public CardFileException(String message) {
+        super(message);
+    }
+
+}
